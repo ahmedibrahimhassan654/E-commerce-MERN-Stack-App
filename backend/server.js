dbd17bf80a55083e1e4645f53e1f63fbc94e4710
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-06-03 23:16:57 +0200

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan =require('morgan')
+const connectDb=require('./config/db')
 
-//routefiles
-const productcategory= require('./routes/productcategory')
+
+
+
 
 
 dotenv.config({ path: './config/config.env' })
 
+
+
+
+connectDb()
+
+//routefiles
+const productcategory= require('./routes/productcategory')
 
 const app = express()
 
@@ -29,9 +38,16 @@ app.use('/api/v1/productcategory', productcategory);
 const PORT = process.env.PORT || 5000
 
 
-app.listen(PORT, () => {
+const server=app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT} in ${process.env.NODE_ENV} Mode`);
 });
 
 
 
+//handle unhandled promise rejections
+process.on('unhandledRejection', (err, Promise) => {
+   console.log(`Error:${err.message}`);
+   
+
+  server.close(() => process.exit(1));
+});
