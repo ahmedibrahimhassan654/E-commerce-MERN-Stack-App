842255e9df43dfa1bd737cfae4f6e51e74312307
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-06-14 00:00:26 +0200

    get me

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -17,7 +17,7 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 // router.get('/logout', logout);
-// router.get('/me', protect, getMe);
+router.get('/me', protect, getMe);
 // router.put('/updatedetails', protect, updateDetails);
 // router.put('/updatepassword', protect, updatePassword);
 // router.post('/forgotpassword', forgotPassword);
